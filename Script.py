d44c9461a8e5f212fb58ec1ecee7376f6850ffd3
PROFESSORSAAB <111982570+PROFESSORSAAB@users.noreply.github.com>
2022-08-26 13:34:56 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,16 +1,16 @@
 class script(object):
     START_TXT = """<b>🔥𝓗𝓲  𝓽𝓱𝓮𝓻𝓮 </b> {}
 
-♨️𝗔𝗱𝗱 𝗧𝗵𝗶𝘀 𝗕𝗼𝘁 𝗧𝗼 𝗬𝗼𝘂𝗿 𝗚𝗿𝗼𝘂𝗽 𝗔𝗻𝗱 𝗚𝗶𝘃𝗲 𝗔𝗱𝗺𝗶𝗻. 𝗙𝗿𝗼𝗺 𝗧𝗵𝗮𝘁 𝗧𝗶𝗺𝗲 𝗧𝗵𝗶𝘀 𝗕𝗼𝘁 𝗪𝗶𝗹𝗹 𝗣𝗿𝗼𝘃𝗶𝗱𝗲 𝗚𝗮𝗺𝗲𝘀<b>,</b>𝗠𝗼𝘃𝗶𝗲𝘀 𝗔𝗻𝗱 𝗦𝗲𝗿𝗶𝗲𝘀 𝗙𝗼𝗿 𝗥𝗲𝗾𝘂𝗲𝘀𝘁𝘀. 𝗧𝗵𝗶𝘀 𝗕𝗼𝘁 𝗛𝗮𝘃𝗲 𝗧𝗵𝗲 𝗟𝗮𝗿𝗴𝗲𝘀𝘁 𝗚𝗮𝗺𝗲𝘀<b>,</b>𝗠𝗼𝘃𝗶𝗲𝘀 𝗔𝗻𝗱 𝗦𝗲𝗿𝗶𝗲𝘀 𝗗𝗮𝘁𝗮𝗯𝗮𝘀𝗲 𝗜𝗻 𝗧𝗲𝗹𝗲𝗴𝗿𝗮𝗺😜
+♨️𝗔𝗱𝗱 𝗧𝗵𝗶𝘀 𝗕𝗼𝘁 𝗧𝗼 𝗬𝗼𝘂𝗿 𝗚𝗿𝗼𝘂𝗽 𝗔𝗻𝗱 𝗚𝗶𝘃𝗲 𝗔𝗱𝗺𝗶𝗻. 𝗙𝗿𝗼𝗺 𝗧𝗵𝗮𝘁 𝗧𝗶𝗺𝗲 𝗧𝗵𝗶𝘀 𝗕𝗼𝘁 𝗪𝗶𝗹𝗹 𝗣𝗿𝗼𝘃𝗶𝗱𝗲 𝗚𝗮𝗺𝗲𝘀<b>,</b>𝗠𝗼𝘃𝗶𝗲𝘀 𝗔𝗻𝗱 𝗦𝗲𝗿𝗶𝗲𝘀 𝗙𝗼𝗿 𝗥𝗲𝗾𝘂𝗲𝘀𝘁𝘀. 𝗧𝗵𝗶𝘀 𝗕𝗼𝘁 𝗛𝗮𝘃𝗲 𝗧𝗵𝗲 𝗟𝗮𝗿𝗴𝗲𝘀𝘁 𝗚𝗮𝗺𝗲𝘀<b>,</b>𝗠𝗼𝘃𝗶𝗲𝘀 𝗔𝗻𝗱 𝗦𝗲𝗿𝗶𝗲𝘀 𝗗𝗮𝘁𝗮𝗯𝗮𝘀𝗲 𝗜𝗻 𝗧𝗲𝗹𝗲𝗴𝗿𝗮𝗺🔰
 
-🔰𝗖𝗹𝗶𝗰𝗸 𝗧𝗵𝗲 𝗔𝗯𝗼𝘂𝘁 𝗕𝘂𝘁𝘁𝗼𝗻 𝗙𝗼𝗿 𝗠𝗼𝗿𝗲 𝗜𝗻𝗳𝗼𝗿𝗺𝗮𝘁𝗶𝗼𝗻🤪
+🔰𝗖𝗹𝗶𝗰𝗸 𝗧𝗵𝗲 𝗔𝗯𝗼𝘂𝘁 𝗕𝘂𝘁𝘁𝗼𝗻 𝗙𝗼𝗿 𝗠𝗼𝗿𝗲 𝗜𝗻𝗳𝗼𝗿𝗺𝗮𝘁𝗶𝗼𝗻🔰
 
 🤖By Using Our Service You Must Agree To Our Privacy Policy 👀
 """
     HELP_TXT = """<b>🔥𝓗𝓲  𝓽𝓱𝓮𝓻𝓮 </b>{}
     
-✨ <b>My Name :</b>  𝗖𝝠𝝦𝝩𝝠𝝞𝝥 𝝦𝗥𝝞𝗖𝝽
-👨‍💻 <b>Developer :</b> 𝙺𝚒𝚜𝚊𝚛𝚊 𝙿𝚎𝚜𝚊𝚗𝚓𝚒𝚝𝚑
+✨ <b>My Name :</b>  PROFESSORSAAB
+👨‍💻 <b>Developer :</b> PROFESSORSAAB
 🍀 <b>Data Base :</b> Mango DB
 📝 <b>Language :</b> Python3
 🧰 <b>Framework :</b> Pyrogram
@@ -26,11 +26,11 @@ class script(object):
 💥<b>Ids & User Info</b>
 💥<b>Ban , Unban Users</b>
 
-⚙️𝗣𝗼𝘄𝗲𝗿𝗱 𝗕𝘆 :- @AnonymousBotsInfinity
+⚙️𝗣𝗼𝘄𝗲𝗿𝗱 𝗕𝘆 :- @PUNJABI_FILMY_PITAARA_786
 """
     ABOUT_TXT = """<b>🔥𝓗𝓲  𝓽𝓱𝓮𝓻𝓮 </b>{}
     
-✨ <b>My Name :</b> 𝗖𝝠𝝦𝝩𝝠𝝞𝝥 𝝦𝗥𝝞𝗖𝝽
+✨ <b>My Name :</b> PROFESSORSAAB
 👨‍💻 <b>Developer :</b> 𝙺𝚒𝚜𝚊𝚛𝚊 𝙿𝚎𝚜𝚊𝚗𝚓𝚒𝚝𝚑
 🍀 <b>Data Base :</b> Mango DB
 📝 <b>Language :</b> Python3
@@ -47,14 +47,14 @@ class script(object):
 💥<b>Ids & User Info</b>
 💥<b>Ban , Unban Users</b>
 
-⚙️𝗣𝗼𝘄𝗲𝗿𝗱 𝗕𝘆 :- @AnonymousBotsInfinity
+⚙️𝗣𝗼𝘄𝗲𝗿𝗱 𝗕𝘆 :- @PUNJABI_FILMY_PITAARA_786
 """
     CREDITS = """<b>Credit 🌟</b>
     
-🔥𝗖𝝠𝝦𝝩𝝠𝝞𝝥 𝝦𝗥𝝞𝗖𝝽 Is A Auto Filter Bot As Well As One Bot In Telegram That Can Download Games,Movies & Series Inline📝
+🔥PROFESSORSAAB Is A Auto Filter Bot As Well As One Bot In Telegram That Can Download Games,Movies & Series Inline📝
 - <b>Developer 👨‍💻</b>
 
-<b>𝙺𝚒𝚜𝚊𝚛𝚊 𝙿𝚎𝚜𝚊𝚗𝚓𝚒𝚝𝚑</b>
+<b>PROFESSORSAAB</b>
 
 - <b>Thanks To 👑</b>
 
@@ -63,4 +63,4 @@ class script(object):
 <b>🏷️ Trojanz</b> for Their Awesome Unlimited Filter Bot And AutoFilterBoT
 <b>🏷️ Senu Gamer Boy</b> And His friend <b>Subinps</b>
 
-⚙️𝗣𝗼𝘄𝗲𝗿𝗱 𝗕𝘆 :- @AnonymousBotsInfinity"""
+⚙️𝗣𝗼𝘄𝗲𝗿𝗱 𝗕𝘆 :- @PUNJABI_FILMY_PITAARA_786"""
