ff72ddd2158e95f4b62ba996b402bcdb6e1c2a88
PROFESSORSAAB <111982570+PROFESSORSAAB@users.noreply.github.com>
2022-08-26 13:20:19 +0530

    Update banned.py

--- plugins/banned.py
+++ plugins/banned.py
@@ -25,7 +25,7 @@ async def ban_reply(bot, message):
 @Client.on_message(filters.group & disabled_group & filters.incoming)
 async def grp_bd(bot, message):
     buttons = [[
-        InlineKeyboardButton('Support', url=f'https://t.me/MaX_Bots_Support')
+        InlineKeyboardButton('Support', url=f'https://t.me/PUNJABI_FILMY_PITAARA_786')
     ]]
     reply_markup=InlineKeyboardMarkup(buttons)
     vazha = await db.get_chat(message.chat.id)
